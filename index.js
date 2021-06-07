15bb9074634ed5c7bcb5044ae7c3c59c264632cd
Chris Hartgerink <chris@libscie.org>
2021-06-07 15:10:49 +0200

    Add release procedure

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 
-const employmentContract = fs.readFileSync('markdown/employment_contract-de-DE.md').toString()
+const employmentContract = fs
+  .readFileSync('markdown/employment_contract-de-DE.md')
+  .toString()
 const imprint = fs.readFileSync('markdown/imprint-en-DE.md').toString()
 
 module.exports = {
